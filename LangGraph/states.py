1ea0f1283ac97336be99c9a3ed732beceb067be9
Rohan <rohangupta112005@gmail.com>
2026-08-28 16:52:33 +0530

    Create States in langgraph

---
+++ LangGraph/states.py
@@ -0,0 +1,50 @@
+#So now we are creating a graph
+# and the first thing is graph
+
+import os
+
+#1) typed DICT (Most Common)
+
+from typing import TypedDict  #-> we can do typecasting
+
+class State(TypedDict):
+    topic : str
+    summary : str
+    score : int
+    
+#2) pydantic approach 
+#it is good at data validation and type checking at 
+#runtime
+
+from pydantic import BaseModel , field_validator
+
+class State(BaseModel):
+    topic : str
+    summary : str = ""
+    score : int
+    
+    @field_validator
+    def score_positive(cls , v):
+        if v < 0:
+            raise ValueError("Score must be positive!")
+ 
+        
+#3) python dataclaseess 
+
+#standard python dataclass but it is used very rarely 
+
+from dataclasses import dataclass, field 
+
+@dataclass
+class State:
+    topic : str = ""
+    summary  : str = ""
+    messages : list = field(default_factory=list)
+
+#4)
+
+from langgraph.graph import MessagesState
+
+class State(MessagesState):
+    user_name :str
+    language : str
